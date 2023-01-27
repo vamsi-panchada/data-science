fc5b44ba0644a6f23e3fa249cc5e1f89d095ec05
vamsi-panchada <vamsivs58@gmail.com>
2023-01-27 12:05:19 +0530

    GUI work

--- projects/chat-bot/gui.py
+++ projects/chat-bot/gui.py
@@ -72,4 +72,23 @@ def bag_of_words(sentence, words, show_details=True):
 
 def predict_class(sentence):
     predicted = bag_of_words(sentence, words, show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list=[]
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
     
+
+def get_response(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+        result = random.choice(i['responses'])
+        break
+    return result
+    
+
